7e4e634d102c27899ea32f19c75b7a0ba11fd469
Kamil Bonar <kamilbr@op.pl>
2023-05-27 23:56:14 +0200

    32.3 szablon maila

--- Spring-Web-master/src/main/java/com/crud/tasks/service/MailCreatorService.java
+++ Spring-Web-master/src/main/java/com/crud/tasks/service/MailCreatorService.java
@@ -8,6 +8,9 @@ import org.springframework.stereotype.Service;
 import org.thymeleaf.TemplateEngine;
 import org.thymeleaf.context.*;
 
+import java.util.ArrayList;
+import java.util.List;
+
 
 @Service
 public class MailCreatorService {
@@ -22,6 +25,14 @@ public class MailCreatorService {
     private TemplateEngine templateEngine;
 
     public String buildTrelloCardEmail(String message) {
+
+        String result = "";
+
+        List<String> functionality = new ArrayList<>();
+        functionality.add("You can manage your tasks");
+        functionality.add("Provides connection with Trello Account");
+        functionality.add("Application allows sending tasks to Trello");
+
         Context context = new Context();
         context.setVariable("message", message);
         context.setVariable("tasks_url", "http://localhost:8888/tasks_frontend/");
@@ -29,6 +40,15 @@ public class MailCreatorService {
         context.setVariable("admin_name", adminConfig.getAdminName());
         context.setVariable("company_details", companyDetailsConfig.getCompanyName() + ", e-mail: " + companyDetailsConfig.getCompanyEmail() + ", phone: "+ companyDetailsConfig.getCompanyPhone());
         context.setVariable("preview_message", "New card in Trello created");
-        return templateEngine.process("mail/created-trello-card-mail", context);
+        context.setVariable("show_button", false);
+        context.setVariable("is_friend", false);
+        context.setVariable("admin_config", adminConfig);
+        context.setVariable("application_functionality", functionality);
+        if (message.contains("New card")) {
+            result = templateEngine.process("mail/created-trello-card-mail.html", context);
+        } else if (message.contains("Currently")) {
+            result = templateEngine.process("mail/information_mail.html", context);
+        }
+        return result;
     }
 }
